84bc837dae77268264ec87f7c79763b22bfb2b1f
Domspace1 <dominik.theuerkauf@gmx.de>
2020-02-15 15:12:04 +0100

    Fixed coin collection error
    Previously each coin would give +2 to collectedCoins. It now adds 0.5 to compensate

--- scripts/smallCoinForeign.gd
+++ scripts/smallCoinForeign.gd
@@ -3,5 +3,5 @@ extends "res://scripts/smallCoin.gd"
 func _process(delta):
 	overlapping = area.get_overlapping_bodies()
 	if overlapping != []:
-		get_parent().collectedCoins += 1
+		get_parent().collectedCoins += 0.5
 		get_parent().moveCoin(self)
