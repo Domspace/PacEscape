6cc69a682f64b9a4fd355e894c513ce2beab9465
Domspace1 <dominik.theuerkauf@gmx.de>
2020-02-16 00:00:59 +0100

    added car respawn
    cars now respawn after 1 second using a timer that is being created within the car script

--- scripts/redCar.gd
+++ scripts/redCar.gd
@@ -1,6 +1,23 @@
 extends Area2D
 
+onready var carTimer = Timer.new()
+
+var defaultx = get_position().x
+var defaulty = get_position().y
+
+func _ready():
+	add_child(carTimer)
+	carTimer.connect("timeout", self, "_on_Timer_timeout")
+	carTimer.set_one_shot(true)
+	carTimer.set_timer_process_mode(0)
+
 func _process(delta):
-	if get_overlapping_bodies() != []:
-		get_parent().eatenCars += 1
-		get_parent().moveCar(self)
+	for bodies in get_overlapping_bodies():
+		if bodies.name == "pacMan":
+			get_parent().eatenCars += 1
+			get_parent().moveCar(self)
+			carTimer.start()
+			print(carTimer.time_left)
+	print(carTimer.time_left)
+func _on_Timer_timeout():
+	position = Vector2(defaultx, defaulty)
