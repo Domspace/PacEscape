24aca9928e308dbb36ae3ff9876fbda34b536eb7
Domspace1 <dominik.theuerkauf@gmx.de>
2020-02-19 18:18:44 +0100

    added "credits"-button functionality
    the credits-button will now take you the credits and the back-button back to the main menu

--- scripts/playButton.gd
+++ scripts/playButton.gd
@@ -8,3 +8,9 @@ func _on_playButton_pressed():
 
 func _on_quitButton_pressed():
 	get_tree().quit()
+
+func _on_creditsButton_pressed():
+	get_node("/root/mainMenu/camera").position = Vector2(0, 608)
+
+func _on_backButton_pressed():
+	get_node("/root/mainMenu/camera").position = Vector2(0, 0)
